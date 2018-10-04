5ae58f3dfde8c8464f4a8cb2e60266b7ec806bb0
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-10-04 13:50:42 +0300

    Factories removed;

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,19 @@
 import beans.Author;
 import beans.Book;
-import dao.DBImplementation;
 import dao.DBIn;
 import dao.DBOut;
-import dao.dao_factories.InFactory;
-import dao.dao_factories.OutFactory;
 import interfaces.DAO;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            DAO in = new InFactory().createIN();
-            DAO out = new OutFactory().createOUT();
+            DAO in = new DBIn();
+            DAO out = new DBOut();
             System.out.println(out.getAuthor("Сергей", "Лукьяненко"));
             System.out.println(out.getBook("Конкуренты"));
             in.addAuthor("Михаил", "Королюк", new ArrayList<>(Collections.singletonList(new Book("Cпасти СССР"))));
